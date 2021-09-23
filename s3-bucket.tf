a908dd8cbb30873a0b347e7389429dc2cde83102
Michel Oosterhof <moosterhof@hashicorp.com>
2021-09-23 05:27:51 +0000

    - -> _

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
   version = "2.2.0"
 
-  bucket-prefix = var.prefix
+  bucket_prefix = var.prefix
   acl    = "private"
 
   versioning = {
